1061ba2fc7e96e0f931e4a5c16a637b4ca3d3824
Dan Crankshaw <dscrankshaw@gmail.com>
2012-11-30 19:23:42 -0500

    Added the first parts of the implementation.

---
+++ hmm_src/src/cs475/hmm/Classify.java
@@ -0,0 +1,10 @@
+package cs475.hmm;
+
+public class Classify {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+
+    }
+
+}
---
+++ hmm_src/src/cs475/hmm/EmissionProbabilities.java
@@ -0,0 +1,6 @@
+package cs475.hmm;
+
+public class EmissionProbabilities {
+
+    HashM
+}
